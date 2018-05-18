3da9de5df39b7e96c5bf9da686c42f8320bae497
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-18 08:36:56 -0700

    filled out createUser functionality

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,6 +5,22 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  User.findOne({ username })
+  .then(user => {
+    if (user) {
+      user.checkPassword(password).then(isPassword => {
+        if (isPassword) {
+          req.username = user.username;
+          res.send('Welcome');
+        } else {
+          res.status(401).send('invalid password');
+        }
+      });
+    } else {
+      res.status(401).send('invalid username');
+    }
+  })
+  .catch(err => res.send(err));
 };
 
 module.exports = {
