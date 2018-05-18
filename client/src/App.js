5a472007bf13ca9740353517ecdc6dcc4646de21
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-18 09:52:43 -0700

    made auth/jokes folders and files/ filled out app.js with routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Route, withRouter } from 'react-router-dom';
+import Signup from './auth/Signup';
+import Signin from './auth/Signin';
+import Jokes from './jokes/Jokes';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1>Welcome User!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route path='/signup' component={Signup} />
+        <Route path='/signin' component={Signin} />
+        <Route path='/jokes' component={Jokes} />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
